ef88fabea63bb58e8f1ae306f9e72af8c44733bb
Norman Mwagi <normanmwagi23@gmail.com>
2025-07-07 07:59:47 +0300

    server update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://jsonserver-6tdl.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
